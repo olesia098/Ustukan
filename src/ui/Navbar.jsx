d89fba3931b8d8624a26505e8bf2e86b59589d9d
olesia098 <olesya.kan.05@mail.ru>
2025-04-29 21:34:52 +0600

    commit

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import logo from "../img/logo.png";
-import '../index.css';
-
+import "../index.css";
 
 const Navbar = () => {
   return (
     <div className="flex items-start justify-between conteiner">
-      <div className="text-[6vh]" style={{ fontFamily: 'Ondine',  color: 'rgb(91, 44, 28)', }} >Меню</div>
-      <div >
+      <div
+        className="text-[5vh]"
+        style={{ fontFamily: "Ondine", color: "rgb(91, 44, 28)", display: "flex" }}
+      >
+        <p>Меню/</p>
+        <p>Домашнее меню</p>
+      </div>
+      <div>
         <img className="h-[10vh]" src={logo} alt="logo" />
       </div>
     </div>
